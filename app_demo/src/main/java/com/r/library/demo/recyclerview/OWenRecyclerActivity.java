8970b512cb96be488bdb9ab38b26e049db7d2ecf
Guanghui Ren <renguanghui@le.com>
2019-06-17 20:19:37 +0800

    RLibrary: Update project.
    Signed-off-by: Guanghui Ren <renguanghui@le.com>

--- app_demo/src/main/java/com/r/library/demo/recyclerview/OWenRecyclerActivity.java
+++ app_demo/src/main/java/com/r/library/demo/recyclerview/OWenRecyclerActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.View;
 
 import com.owen.tvrecyclerview.widget.TvRecyclerView;
